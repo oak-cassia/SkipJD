110fe3a5bb0979f708ce65dcf31f0a70add7f05e
oak_cassia <gnlckswjd1@gmail.com>
2026-03-17 22:28:27 +0900

    feat: add GetUserByEmail method to UserStore interface

--- internal/service/user_store.go
+++ internal/service/user_store.go
@@ -5,6 +5,7 @@ import "skipjd/internal/model"
 type UserStore interface {
 	ListUsers() ([]model.User, error)
 	GetUserByID(id uint) (*model.User, error)
+	GetUserByEmail(email string) (*model.User, error)
 	CreateUser(user *model.User) error
 	UpdateUser(user *model.User) error
 	DeleteUser(id uint) error
